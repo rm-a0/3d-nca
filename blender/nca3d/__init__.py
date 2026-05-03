c8058e4046796f2cb3d48317ccb23fb01779e74f
rm-a0 <michalrepcik9@gmail.com>
2026-05-03 15:38:48 +0200

    chore: restructure blender add on to better fit required blender add-on structure

--- blender/nca3d/__init__.py
+++ blender/nca3d/__init__.py
@@ -11,7 +11,7 @@ from . import properties, operators, panels
 bl_info = {
     "name": "3D NCA Simulator",
     "author": "Michal Repcik",
-    "version": (1, 1, 0),
+    "version": (0, 1, 0),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar > NCA",
     "description": "3D Neural Cellular Automata Simulator",
